1ae1428b35808569763cb1671c9dcfd610ec3963
Ame_x Edam <edame8080@gmail.com>
2024-06-06 10:16:11 +0000

    add

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /**
- * Superset of TypeScript
+ * SuperSet of TypeScript
  * @module
  */
 
@@ -19,11 +19,47 @@ export type $not<Condition extends boolean, Truthy = true, Falsy = false> = $ext
 // String Transformations types
 export namespace $string {
     export type $concat<Left extends string, Right extends string> = `${Left}${Right}`;
+    
     export type $replace<String extends string, Search extends string, Replacement extends string> = String extends `${infer Left}${Search}${infer Right}`? `${Left}${Replacement}${Right}` : String;
     type _replaceAllHelper<String extends string, Search extends string, Replacement extends string> = String extends `${infer Left}${Search}${infer Right}` ? `${Left}${_replaceAllHelper<Right, Search, Replacement>}${Replacement}` : String;
     export type $replaceAll<String extends string, Search extends string, Replacement extends string> = $if<$equal<String, $replace<String, Search, Replacement>>, String, _replaceAllHelper<$replace<String, Search, Replacement>, Search, Replacement>>;
+    
+    export type $trimStart<String extends string> = String extends ` ${infer Rest}` ? $trimStart<Rest> : String;
+    export type $trimEnd<String extends string> = String extends `${infer Rest} ` ? $trimEnd<Rest> : String;
+    type _trimHelper<String extends string> = $trimEnd<$trimStart<String>>;
+    export type $trim<String extends string> = $if<$equal<String, _trimHelper<String>>, String, _trimHelper<String>>;
+    
+    type _tuple_length<Tuple extends any[]> = Tuple['length'];
+    export type $repeatHelper<String extends string, Times extends number, Result extends string, Tuple extends any[]> =
+        _tuple_length<Tuple> extends Times
+        ? Result
+        : $repeatHelper<String, Times, `${Result}${String}`, [String, ...Tuple]>;
+    export type $repeat<String extends string, Times extends number> = $repeatHelper<String, Times, '', []>;
+
+    export type $toLowerCase<Str extends string> = Str extends `${infer Head}${infer Tail}`
+        ? `${Head extends Uppercase<Head> ? Lowercase<Head> : Head}${$toLowerCase<Tail>}`
+        : Str;
+
+    export type $toUpperCase<Str extends string> = Str extends `${infer Head}${infer Tail}`
+        ? `${Head extends Lowercase<Head> ? Uppercase<Head> : Head}${$toUpperCase<Tail>}`
+        : Str;
+    
+    export type $split<Str extends string, Separator extends string> = Str extends `${infer Head}${Separator}${infer Tail}` ? [Head,...$split<Tail, Separator>] : [Str];
+
+    export type $charAt<Str extends string, Index extends number, Tuple extends any[] = []> =
+        Str extends `${infer Head}${infer Tail}`
+        ? _tuple_length<Tuple> extends Index
+          ? Head
+          : $charAt<Tail, Index, [...Tuple, any]>
+        : Str;
 }
 
 
 type a = $string.$replace<'1231', '1', '2'>;
 type b = $string.$replaceAll<'12311', '1', '2'>;
+type c = $string.$trim<' 1231  '>;
+type d = $string.$repeat<'1', 3>;
+type e = $string.$toLowerCase<'1231abcD'>;
+type f = $string.$toUpperCase<'1231abcD'>;
+type g = $string.$split<'a1231abcD', '1'>;
+type h = $string.$charAt<'1231abcD', 1>;
