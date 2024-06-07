b0212a145d49c00f5c070437277e0180939f261f
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2024-06-07 23:54:08 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -226,39 +226,33 @@ export namespace $number {
     Value[number] extends _plusSign ? Value : $new<Value["length"], _plusSign>;
 
   type _addHelper<
-    Value extends _plusSign[] | _minusSign[],
-    Other extends _plusSign[] | _minusSign[],
-    Result extends any[] = Value
-  > = Value extends [...infer Head, infer Rest]
-    ? Head extends _plusSign[]
-      ? Other extends [...infer OtherHead, infer OtherRest]
-        ? _addHelper<
-            Head,
-            OtherHead,
-            [
-              ...(Result extends [...infer ResultHead, infer ResultRest]
-                ? ResultHead
-                : [])
-            ]
-          >
-        : Result
-      : _addHelper<Other, Value>
-    : Other;
+  Value extends _plusSign[] | _minusSign[],
+  Other extends _plusSign[] | _minusSign[],
+  Result extends any[] = Value
+> = Value extends [...infer Head, infer Rest]
+  ? Head extends _plusSign[]
+    ? Other extends [...infer OtherHead, infer OtherRest]
+      ? _addHelper<
+          Head,
+          OtherHead extends _plusSign[] | _minusSign[] ? OtherHead : [],
+          [...(Result extends [...infer ResultHead, infer ResultRest]
+            ? ResultHead
+            : [])]
+        >
+      : Result
+    : _addHelper<Other, Value>
+  : Other
 
   export type $add<
     Value extends _plusSign[] | _minusSign[],
     Other extends _plusSign[] | _minusSign[]
-  > = Value extends []
-    ? Other extends []
-      ? []
-      : $if<
-          $equal<Value[number], Other[number]>,
-          [...Value, ...Other],
-          _addHelper<Value, Other>
-        >
-    : $if<
-        $equal<Value[number], Other[number]>,
-        [...Value, ...Other],
-        _addHelper<Value, Other>
-      >;
+  > = Value extends [] ? Other extends [] ? [] : $if<
+    $equal<Value[number], Other[number]>,
+    [...Value, ...Other],
+    _addHelper<Value, Other>
+  > : $if<
+    $equal<Value[number], Other[number]>,
+    [...Value, ...Other],
+    _addHelper<Value, Other>
+  > 
 }
